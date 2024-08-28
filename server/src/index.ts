bf22e4498e8e48bc979c1a0330b5e1842bc400e2
Mertcan Öncül <mertcanoncul@icloud.com>
2024-08-28 13:20:47 +0300

    basic express server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!")
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page!</h1>")
+})
+
+app.listen(port, () =>
+  console.log(`Server is running on port http://localhost:${port}`)
+)
